b396addbf4c4c53806dbd9c2b73cf13da92c8da0
ViYeM <vitoria.miao@sga.pucminas.br>
2023-08-31 00:11:52 -0300

    1

---
+++ src/main/java/src/relogio/Relogio.java
@@ -0,0 +1,23 @@
+package relogio;
+
+public class Relogio {
+    private int horas;
+    private int minutos;
+    private int segundos;
+
+    public void programarHorario(int horas, int minutos, int segundos) {
+        this.horas = horas;
+        this.minutos = minutos;
+        this.segundos = segundos;
+    }
+
+    public void reiniciarMeiaNoite() {
+        this.horas = 0;
+        this.minutos = 0;
+        this.segundos = 0;
+    }
+
+    public String obterHorario() {
+        return String.format("%02d:%02d:%02d", horas, minutos, segundos);
+    }
+}
